1fc14e153cc09219d57377d18761b78a183d72e8
nurulhasan2k <nurulhasan2k@users.noreply.github.com>
2020-08-07 11:02:07 +0600

    mysnox

---
+++ js/tab_pill.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.pill_signup_li').click(function(){
+		$('.hr_1').hide();
+	});
+	$('.pill_signup_li').click(function(){
+		$('.hr_2').show();
+	})
+	$('.pill_login_li').click(function(){
+		$('.hr_2').hide();
+	});
+	$('.pill_login_li').click(function(){
+		$('.hr_1').show();
+	})
+})
